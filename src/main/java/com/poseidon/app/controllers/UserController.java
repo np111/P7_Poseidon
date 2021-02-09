47d276a864db9d2dbe1b2954db39cd87cf9aba39
np111 <oc.np111@protonmail.com>
2021-02-09 16:23:13 +0100

    Update UserController to use UserService

--- src/main/java/com/poseidon/app/controllers/UserController.java
+++ src/main/java/com/poseidon/app/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.poseidon.app.controllers;
 
-import com.poseidon.app.persistence.entity.UserEntity;
-import com.poseidon.app.persistence.repository.UserRepository;
+import com.poseidon.app.model.User;
+import com.poseidon.app.service.UserService;
+import java.util.List;
 import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,62 +16,75 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Controller
+@Scope("singleton")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @RequestMapping("/user/list")
     public String home(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", filterOut(userService.list()));
         return "user/list";
     }
 
     @GetMapping("/user/add")
-    public String addUser(UserEntity bid) {
+    public String addUser(User user) {
         return "user/add";
     }
 
     @PostMapping("/user/validate")
-    public String validate(@Valid UserEntity user, BindingResult result, Model model) {
-        if (!result.hasErrors()) {
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            user.setPassword(encoder.encode(user.getPassword()));
-            userRepository.save(user);
-            model.addAttribute("users", userRepository.findAll());
-            return "redirect:/user/list";
+    public String validate(@Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "user/add";
         }
-        return "user/add";
+
+        userService.create(filterIn(user));
+        model.addAttribute("users", filterOut(userService.list()));
+        return "redirect:/user/list";
     }
 
     @GetMapping("/user/update/{id}")
     public String showUpdateForm(@PathVariable("id") Long id, Model model) {
-        UserEntity user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        user.setPassword("");
-        model.addAttribute("user", user);
+        model.addAttribute("user", filterOut(userService.read(id)));
         return "user/update";
     }
 
     @PostMapping("/user/update/{id}")
-    public String updateUser(@PathVariable("id") Long id, @Valid UserEntity user,
-            BindingResult result, Model model) {
+    public String updateUser(@PathVariable("id") Long id, @Valid User user, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "user/update";
         }
 
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        user.setPassword(encoder.encode(user.getPassword()));
-        user.setId(id);
-        userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
+        userService.update(id, filterIn(user));
+        model.addAttribute("users", filterOut(userService.list()));
         return "redirect:/user/list";
     }
 
     @GetMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") Long id, Model model) {
-        UserEntity user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        userRepository.delete(user);
-        model.addAttribute("users", userRepository.findAll());
+        userService.delete(id);
+        model.addAttribute("users", filterOut(userService.list()));
         return "redirect:/user/list";
+    }
+
+    private User filterIn(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return user;
+    }
+
+    private User filterOut(User user) {
+        if (user.getPassword() != null) {
+            user.setPassword("");
+        }
+        return user;
+    }
+
+    private List<User> filterOut(List<User> ret) {
+        if (ret != null) {
+            ret.forEach(this::filterOut);
+        }
+        return ret;
     }
 }
--- src/main/java/com/poseidon/app/model/User.java
+++ src/main/java/com/poseidon/app/model/User.java
@@ -1,5 +1,9 @@
 package com.poseidon.app.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,8 +16,20 @@ import lombok.NoArgsConstructor;
 @Data
 public class User {
     private Long id;
+
+    @NotBlank(message = "Username is mandatory")
     private String username;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = ".*\\p{Lu}.*", message = "Password must contain at least one uppercase letter.")
+    @Pattern(regexp = ".*[0-9].*", message = "Password must contain at least one number.")
+    @Pattern(regexp = ".*[^\\p{L}0-9].*", message = "Password must contain at least one symbol.")
     private String password;
+
+    @NotBlank(message = "Full Name is mandatory")
     private String fullname;
+
+    @NotNull(message = "Role is mandatory")
     private Role role;
 }

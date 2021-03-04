59a5346336399159bc1beb2041c8eaa6e5f0b508
np111 <oc.np111@protonmail.com>
2021-03-04 17:17:23 +0100

    Javadoc CrudService & CrudMapper

--- src/main/java/com/poseidon/app/persistence/mapper/CrudMapper.java
+++ src/main/java/com/poseidon/app/persistence/mapper/CrudMapper.java
@@ -2,10 +2,34 @@ package com.poseidon.app.persistence.mapper;
 
 import org.mapstruct.MappingTarget;
 
+/**
+ * Mapper used to transform an entity into its model (and vice versa).
+ *
+ * @param <Model>  The model (eg. a DTO)
+ * @param <Entity> The entity (eg. a JPA entity)
+ */
 public interface CrudMapper<Model, Entity> {
+    /**
+     * Transforms an entity into the corresponding model.
+     *
+     * @param entity an entity
+     * @return a corresponding model
+     */
     Model toModel(Entity entity);
 
+    /**
+     * Transforms a model into the corresponding entity.
+     *
+     * @param model a model
+     * @return a corresponding entity
+     */
     Entity fromModel(Model model);
 
+    /**
+     * Transforms a model into the corresponding entity.
+     *
+     * @param targetEntity the target entity, which will become a corresponding entity of the model
+     * @param model        a model
+     */
     void fromModel(@MappingTarget Entity targetEntity, Model model);
 }
--- src/main/java/com/poseidon/app/service/CrudService.java
+++ src/main/java/com/poseidon/app/service/CrudService.java
@@ -8,26 +8,93 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Generic service to manage the CRUD operations of a model (DTO) and store it as a JPA entity.
+ *
+ * @param <Model>  The model (DTO)
+ * @param <Entity> The JPA entity
+ * @param <Id>     The type of primary ID of the entity
+ */
 public abstract class CrudService<Model, Entity, Id> {
-    private static final Sort SORT_ID_ASC = Sort.by("id").ascending();
+    private final Sort defaultSort;
 
+    public CrudService() {
+        defaultSort = getDefaultSort();
+    }
+
+    /**
+     * Returns the repository used to access and store the {@linkplain Entity JPA entities}.
+     *
+     * @return the repository
+     */
     protected abstract JpaRepository<Entity, Id> getRepository();
 
+    /**
+     * Returns the mapper used to convert {@linkplain Model models} into {@linkplain Entity entities} (and vice versa).
+     *
+     * @return the mapper
+     */
     protected abstract CrudMapper<Model, Entity> getMapper();
 
+    /**
+     * Set the model ID.
+     * <p>
+     * Should probably be implemented as: <pre>{@code model.setId(id)}</pre>
+     *
+     * @param model The model whose ID will be modified
+     * @param id    The ID to set
+     */
     protected abstract void setModelId(Model model, Id id);
 
+    /**
+     * Returns the default sorting applied when returning an entity list.
+     *
+     * @return the default sort
+     */
+    protected Sort getDefaultSort() {
+        return Sort.by("id").ascending();
+    }
+
+    /**
+     * This method is called at the time of the CREATE operation, just before mapping the {@linkplain Model model} to an
+     * {@linkplain Entity entity}.
+     * <p>
+     * You can therefore apply changes to this new model before the entity is created.
+     *
+     * @param newModel The new model that will be created
+     */
     protected void prepareCreate(Model newModel) {
     }
 
+    /**
+     * This method is called at the time of the UPDATE operation, just before mapping the {@linkplain Model model} to an
+     * {@linkplain Entity entity}.
+     * <p>
+     * You can therefore apply changes to this new model before the entity is created.
+     *
+     * @param newModel      The new model that will be created
+     * @param currentEntity The existing entity that will be replaced.
+     */
     protected void prepareUpdate(Model newModel, Entity currentEntity) {
     }
 
+    /**
+     * Returns a list of all the entities in the repository.
+     *
+     * @return a list of all the entities
+     */
     @Transactional(readOnly = true)
     public List<Model> list() {
-        return getRepository().findAll(SORT_ID_ASC).stream().map(getMapper()::toModel).collect(Collectors.toList());
+        return getRepository().findAll(defaultSort).stream().map(getMapper()::toModel).collect(Collectors.toList());
     }
 
+    /**
+     * Returns the entity with the specified ID.
+     *
+     * @param id the ID of the entity to be returned
+     * @return the entity with the specified ID
+     * @throws EntityNotFoundException if no entity match this ID
+     */
     @Transactional(readOnly = true)
     public Model read(Id id) {
         Entity entity = getRepository().findById(id).orElse(null);
@@ -37,6 +104,14 @@ public abstract class CrudService<Model, Entity, Id> {
         return getMapper().toModel(entity);
     }
 
+    /**
+     * Creates a new entity.
+     * <p>
+     * If set, the model ID will be ignored. A new ID will be generated each time (according to the repository method).
+     *
+     * @param model the model of the new entity to create
+     * @return the model of the created entity (with ID defined)
+     */
     @Transactional
     public Model create(Model model) {
         setModelId(model, null);
@@ -46,6 +121,16 @@ public abstract class CrudService<Model, Entity, Id> {
         return getMapper().toModel(entity);
     }
 
+    /**
+     * Update an existing entity.
+     * <p>
+     * If set, the model ID will be ignored. The current entity ID will always be kept.
+     *
+     * @param id    the ID of the existing entity to update
+     * @param model the new model
+     * @return the model of the updated entity
+     * @throws EntityNotFoundException if no entity match this ID
+     */
     @Transactional
     public Model update(Id id, Model model) {
         Entity entity = getRepository().findById(id).orElse(null);
@@ -59,6 +144,12 @@ public abstract class CrudService<Model, Entity, Id> {
         return getMapper().toModel(entity);
     }
 
+    /**
+     * Delete an entity.
+     *
+     * @param id the ID of the existing entity to delete
+     * @throws EntityNotFoundException if no entity match this ID
+     */
     @Transactional
     public void delete(Id id) {
         Entity entity = getRepository().findById(id).orElse(null);
